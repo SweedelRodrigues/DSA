af47159ed013a730f2fef3360e856f707ceaccb5
Sweedel Rodrigues <sweedel171@gmail.com>
2026-05-25 20:51:40 +0530

    added leetcode 1910

---
+++ removeoccofsubstring_1910.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+
+    string removeOccurrences(string s, string part) {
+
+        while(s.length() > 0 &&
+              s.find(part) < s.length()) {
+
+            s.erase(s.find(part), part.length());
+        }
+
+        return s;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    string s, part;
+
+    cout << "Enter main string: ";
+    getline(cin, s);
+
+    cout << "Enter substring to remove: ";
+    getline(cin, part);
+
+    string ans = obj.removeOccurrences(s, part);
+
+    cout << "Final String: " << ans << endl;
+
+    return 0;
+}
