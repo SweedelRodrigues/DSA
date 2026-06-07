d924f8c3874f01351c72e6aa329a1161eba35786
Sweedel Rodrigues <sweedel171@gmail.com>
2026-06-07 21:58:08 +0530

    added leetcode 1929

---
+++ concatenation_1929.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> getConcatenation(vector<int>& nums) {
+    vector<int> ans;
+
+    for (int i = 0; i < nums.size(); i++) {
+        ans.push_back(nums[i]);
+    }
+
+    for (int i = 0; i < nums.size(); i++) {
+        ans.push_back(nums[i]);
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 1};
+
+    vector<int> result = getConcatenation(nums);
+
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+
+    return 0;
+}
