000e3548753bbcd2e0fee7d5ecc40dcfe73975df
Sweedel Rodrigues <sweedel171@gmail.com>
2026-06-21 22:01:36 +0530

    Time: 0 ms (100%), Space: 31.2 MB (80.85%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int binsearch(vector<int>&arr,int tar,int st,int end)
+    {
+        if(st<=end)
+        {
+            int mid=st+(end-st)/2;
+            if(arr[mid]==tar) return mid;
+            else if(arr[mid]<=tar)
+            {
+                return binsearch(arr,tar,mid+1,end);
+            }else 
+            {
+                return binsearch(arr,tar,st,mid-1);
+            }
+        }
+        return -1;
+    }
+    int search(vector<int>& arr, int tar) {
+        return binsearch(arr,tar,0,arr.size()-1);
+    }
+};
