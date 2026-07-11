c3943a6b7d09b00bc44ba29f423c1a13c32a3327
Sweedel Rodrigues <sweedel171@gmail.com>
2026-07-11 10:25:13 +0530

    Refactor isSubsequence method for clarity

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,15 +1,18 @@
 class Solution {
-    public:
-        bool isSubsequence(string s, string t) {
-                int i = 0, j = 0;
+public:
+    bool isSubsequence(string s, string t) {
 
-                        while (i < s.size() && j < t.size()) {
-                                    if (s[i] == t[j]) {
-                                                    i++;
-                                                                }
-                                                                            j++;
-                                                                                    }
+        int i = 0, j = 0;
 
-                                                                                            return i == s.size();
-                                                                                                }
-                                                                                                };
+        while (i < s.size() && j < t.size()) {
+
+            if (s[i] == t[j]) {
+                i++;
+            }
+
+            j++;
+        }
+
+        return i == s.size();
+    }
+};
