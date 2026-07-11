1dcdca44b0b94381a403ef825e550caf199d14a1
Sweedel Rodrigues <sweedel171@gmail.com>
2026-07-11 13:47:21 +0530

    Time: 0 ms (100%), Space: 8.8 MB (12.52%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    public:
+
+        string buildString(string str) {
+
+                stack<char> st;
+
+                        for (char ch : str) {
+
+                                    if (ch == '#') {
+
+                                                    if (!st.empty()) {
+                                                                        st.pop();
+                                                                                        }
+                                                                                                    }
+                                                                                                                else {
+                                                                                                                                st.push(ch);
+                                                                                                                                            }
+                                                                                                                                                    }
+
+                                                                                                                                                            string ans = "";
+
+                                                                                                                                                                    while (!st.empty()) {
+                                                                                                                                                                                ans += st.top();
+                                                                                                                                                                                            st.pop();
+                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                            reverse(ans.begin(), ans.end());
+
+                                                                                                                                                                                                                    return ans;
+                                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                            bool backspaceCompare(string s, string t) {
+
+                                                                                                                                                                                                                                    return buildString(s) == buildString(t);
+                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                        };
