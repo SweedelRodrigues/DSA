cb858911f84338b448fbfc0d2800b5b49c1f72b2
Sweedel Rodrigues <sweedel171@gmail.com>
2026-07-12 18:59:13 +0530

    Refactor removeElement method formatting

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,17 +1,17 @@
 class Solution {
-    public:
-        int removeElement(vector<int>& nums, int val) {
+public:
+    int removeElement(vector<int>& nums, int val) {
 
-                int k = 0;
+        int k = 0;
 
-                        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) {
 
-                                    if (nums[i] != val) {
-                                                    nums[k] = nums[i];
-                                                                    k++;
-                                                                                }
-                                                                                        }
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
 
-                                                                                                return k;
-                                                                                                    }
-                                                                                                    };
+        return k;
+    }
+};
