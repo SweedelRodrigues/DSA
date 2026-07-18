a9d14deb2edbabb3a59391c87335538217c14e48
Sweedel Rodrigues <sweedel171@gmail.com>
2026-07-18 20:14:50 +0530

    Time: 0 ms (100%), Space: 23.7 MB (83.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
-        vector<int> ans;
-
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] != 0) {
-                ans.push_back(nums[i]);
+        int n=nums.size();
+        int k=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[k]=nums[i];
+                k++;
             }
         }
-
-        int rem = nums.size() - ans.size();
-
-        for(int i = 0; i < rem; i++) {
-            ans.push_back(0);
+        for(int i=k;i<n;i++)
+        {
+            nums[i]=0;
         }
-
-        nums = ans;
     }
 };
