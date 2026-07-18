86c69e36d4acb9d2708f360a62c13db07125acfe
Sweedel Rodrigues <sweedel171@gmail.com>
2026-07-18 16:14:07 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.45%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    public:
+        bool isAnagram(string s, string t) {
+
+                if (s.size() != t.size()) {
+                            return false;
+                                    }
+
+                                            unordered_map<char, int> m;
+
+                                                    // Count frequency of characters in s
+                                                            for (int i = 0; i < s.size(); i++) {
+                                                                        m[s[i]]++;
+                                                                                }
+
+                                                                                        // Decrease frequency using t
+                                                                                                for (int i = 0; i < t.size(); i++) {
+                                                                                                            m[t[i]]--;
+
+                                                                                                                        if (m[t[i]] < 0) {
+                                                                                                                                        return false;
+                                                                                                                                                    }
+                                                                                                                                                            }
+
+                                                                                                                                                                    return true;
+                                                                                                                                                                        }
+                                                                                                                                                                        };
