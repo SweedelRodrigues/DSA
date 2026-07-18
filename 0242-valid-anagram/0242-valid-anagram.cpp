83b34164c58740d0595f6c78e54ddbf26ede02e5
Sweedel Rodrigues <sweedel171@gmail.com>
2026-07-18 17:36:55 +0530

    Time: 3 ms (45.96%), Space: 9.9 MB (26.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,27 @@
 class Solution {
-public:
-    bool isAnagram(string s, string t) {
+    public:
+        bool isAnagram(string s, string t) {
 
-        if (s.size() != t.size()) {
-            return false;
-        }
+                if (s.size() != t.size()) {
+                            return false;
+                                    }
 
-        unordered_map<char, int> m;
+                                            unordered_map<char, int> m;
 
-        // Count frequency of characters in s
-        for (int i = 0; i < s.size(); i++) {
-            m[s[i]]++;
-        }
+                                                    // Count frequency of characters in s
+                                                            for (int i = 0; i < s.size(); i++) {
+                                                                        m[s[i]]++;
+                                                                                }
 
-        // Decrease frequency using t
-        for (int i = 0; i < t.size(); i++) {
-            m[t[i]]--;
+                                                                                        // Decrease frequency using t
+                                                                                                for (int i = 0; i < t.size(); i++) {
+                                                                                                            m[t[i]]--;
 
-            if (m[t[i]] < 0) {
-                return false;
-            }
-        }
+                                                                                                                        if (m[t[i]] < 0) {
+                                                                                                                                        return false;
+                                                                                                                                                    }
+                                                                                                                                                            }
 
-        return true;
-    }
-};
+                                                                                                                                                                    return true;
+                                                                                                                                                                        }
+                                                                                                                                                                        };
