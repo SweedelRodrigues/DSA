09f39d87a26e961346f46206ff91aa12f46b0c87
Sweedel Rodrigues <sweedel171@gmail.com>
2026-07-19 15:43:28 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.66%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int binsearch(vector<int>&arr,int tar,int st,int end)
-    {
-        if(st<=end)
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int st=0;
+        int end=n-1;
+        while(st<=end)
         {
-            int mid=st+(end-st)/2;
-            if(arr[mid]==tar) return mid;
-            else if(arr[mid]<=tar)
+             int mid=st+(end-st)/2;
+            if(nums[mid]==target)
             {
-                return binsearch(arr,tar,mid+1,end);
-            }else 
+                return mid;
+            }
+             else if(nums[mid]<target)
             {
-                return binsearch(arr,tar,st,mid-1);
+                st=mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
             }
         }
         return -1;
-    }
-    int search(vector<int>& arr, int tar) {
-        return binsearch(arr,tar,0,arr.size()-1);
     }
 };
