d0338ea8cb90a5bd86a273f6b0786ecd9593fe5b
Sweedel Rodrigues <sweedel171@gmail.com>
2026-07-20 12:32:25 +0530

    Refactor findMaxAverage for improved readability

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,22 +1,22 @@
 class Solution {
-    public:
-        double findMaxAverage(vector<int>& nums, int k) {
-                int n = nums.size();
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
 
-                        // Compute sum of first window
-                                double sum = 0;
-                                        for (int i = 0; i < k; i++) {
-                                                    sum += nums[i];
-                                                            }
+        // Compute sum of first window
+        double sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
 
-                                                                    double maxSum = sum;
+        double maxSum = sum;
 
-                                                                            // Slide the window
-                                                                                    for (int i = k; i < n; i++) {
-                                                                                                sum = sum - nums[i - k] + nums[i];
-                                                                                                            maxSum = max(maxSum, sum);
-                                                                                                                    }
+        // Slide the window
+        for (int i = k; i < n; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            maxSum = max(maxSum, sum);
+        }
 
-                                                                                                                            return maxSum / k;
-                                                                                                                                }
-                                                                                                                                };
+        return maxSum / k;
+    }
+};
