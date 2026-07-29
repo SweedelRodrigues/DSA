f24b0edbd97a32f52d4c2fbb1fd8798cc64c51da
Sweedel Rodrigues <sweedel171@gmail.com>
2026-07-29 11:15:11 +0530

    Time: 0 ms (100%), Space: 20.7 MB (59.27%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int val:nums)
-        {
-            ans=ans^val;
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans = ans ^ nums[i];
         }
+
         return ans;
     }
-    
 };
