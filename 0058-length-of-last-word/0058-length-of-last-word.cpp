fb19a65a34b95de3e9f118256a722c3c1ab99e05
Sweedel Rodrigues <sweedel171@gmail.com>
2026-08-06 22:55:49 +0530

    Time: 0 ms (100%), Space: 9 MB (34.83%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i = s.length() - 1;
+
+        int i = s.size() - 1;
 
         // Skip trailing spaces
-        while (i >= 0 && s[i] == ' ')
+        while(i >= 0 && s[i] == ' ')
+        {
             i--;
+        }
 
         int count = 0;
 
-        // Count last word length
-        while (i >= 0 && s[i] != ' ') {
+        // Count characters of the last word
+        while(i >= 0 && s[i] != ' ')
+        {
             count++;
             i--;
         }
