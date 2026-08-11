7a720d2af4bc2b66e054581a8f017099085ac303
Sweedel Rodrigues <sweedel171@gmail.com>
2026-08-11 10:40:04 +0530

    Time: 0 ms (100%), Space: 8.7 MB (72.35%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,27 +1,39 @@
-class Solution { 
-public: 
-    bool wordPattern(string pattern, string s) { 
-        vector<string> words; 
-        stringstream ss(s); 
-        string word; 
-        while (ss >> word) { 
-            words.push_back(word); 
-        } 
-        if (pattern.length() != words.size()) 
-            return false; 
-        unordered_map<char, string> m1; 
-        unordered_map<string, char> m2; 
-        for (int i = 0; i < pattern.length(); i++) { 
-            if (m1.find(pattern[i]) != m1.end()) { 
-                if (m1[pattern[i]] != words[i]) 
-                    return false; 
-            } 
-            if (m2.find(words[i]) != m2.end()) { 
-                if (m2[words[i]] != pattern[i]) 
-                    return false; 
-            } 
-          m1[pattern[i]] = words[i]; m2[words[i]] = pattern[i];
-          } 
-        return true;
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string>words;
+        stringstream ss(s);
+        string word;
+        while(ss>>word)
+        {
+            words.push_back(word);
         }
-    };
+        unordered_map<char,string>m1;
+        unordered_map<string,char>m2;
+        if (pattern.length() != words.size())
+        {
+           return false;
+        }
+        for(int i=0;i<pattern.length();i++)
+        {
+            if(m1.find(pattern[i])!=m1.end())
+            {
+                if(m1[pattern[i]]!=words[i])
+                {
+                    return false;
+                }
+            }
+            m1[pattern[i]]=words[i];
+            if(m2.find(words[i])!=m2.end())
+            {
+                if(m2[words[i]]!=pattern[i])
+                {
+                    return false;
+                }
+            }
+            m2[words[i]]=pattern[i];
+
+        }
+        return true;
+    }
+};
