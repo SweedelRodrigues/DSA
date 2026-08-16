914c8de4d140be459db5906fdb245b1ceed8ab01
Sweedel Rodrigues <sweedel171@gmail.com>
2026-08-16 11:07:40 +0530

    Time: 0 ms (100%), Space: 35.7 MB (79.49%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0;
+
+        // Calculate total sum
+        for (int num : nums) {
+            total += num;
+        }
+
+        int leftSum = 0;
+
+        // Find pivot
+        for (int i = 0; i < nums.size(); i++) {
+
+            int rightSum = total - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
